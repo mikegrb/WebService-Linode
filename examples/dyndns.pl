defabe33b579a7cd35fc44df7642a5aac818e5d3
mgreb <mgreb@ad6a8d7d-ba07-0410-ad06-bed234791a8b>
2008-06-11 18:17:47 +0000

    dyndns.pl forgot svnadd
    git-svn-id: svn+ssh://svn.linode.com/srv/linode/branches/api/modules/WebService-Linode@1343 ad6a8d7d-ba07-0410-ad06-bed234791a8b

---
+++ examples/dyndns.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use WebService::Linode;
+use LWP::Simple;
+
+# yourname.com is a master zone with a resource record of type A named home
+# that should point to home IP.
+
+my $apikey = '';
+my $domain = 'yourname.com';
+my $record = 'home';
+
+# get public ip
+chomp(my $pubip = get('http://ip.thegrebs.com/'));
+
+my $api = new WebService::Linode( apikey => $apikey );
+
+my $resourceid =
+	$api->getResourceIDbyName(domain => $domain, name => $record);
+
+die "Couldn't find RR id" unless $resourceid;
+
+$api->domainResourceUpdate(resourceid => $resourceid, target => $pubip);
