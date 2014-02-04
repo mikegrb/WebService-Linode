06b8a3deded3ad5f7c1fa3b924a74ce0af6fc252
trevorparker <trevor@trevorparker.com>
2014-02-04 11:34:18 -0500

    API 2FA support See <https://blog.linode.com/2014/02/03/linode-api-multiple-keys-and-two-factor/>

--- lib/WebService/Linode.pm
+++ lib/WebService/Linode.pm
@@ -151,7 +151,9 @@ my %validation = (
         ],
     },
     test => { echo => [ [], [] ], },
-    user => { getapikey => [ [ 'password', 'username' ], [] ], },
+    user => {
+        getapikey => [ [qw( password username )], [qw( expires label token )] ],
+    },
 );
 
 sub AUTOLOAD {
@@ -1236,6 +1238,18 @@ Required Parameters:
 =item * password
 
 =item * username
+
+=back
+
+Optional Parameters:
+
+=over 4
+
+=item * expires
+
+=item * label
+
+=item * token
 
 =back
 
