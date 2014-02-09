09ae627ef0123b5ebe6c37cb143c9c326e52dac6
mikegrb <mgreb@linode.com>
2014-02-09 00:28:34 -0500

    0.19

--- lib/WebService/Linode.pm
+++ lib/WebService/Linode.pm
@@ -9,7 +9,7 @@ use Carp;
 use List::Util qw(first);
 use WebService::Linode::Base;
 
-our $VERSION = '0.18';
+our $VERSION = '0.19';
 our @ISA     = ("WebService::Linode::Base");
 our $AUTOLOAD;
 
--- lib/WebService/Linode/DNS.pm
+++ lib/WebService/Linode/DNS.pm
@@ -12,7 +12,7 @@ WebService::Linode::DNS - Deprecated Perl Interface to the Linode.com API DNS me
 
 =cut
 
-our $VERSION = '0.18';
+our $VERSION = '0.19';
 our @ISA = ("WebService::Linode::Base");
 
 sub getDomainIDbyName {
